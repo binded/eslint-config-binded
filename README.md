0463e6aa765669136b2266471c3db8376bf8a7a6
Olivier Lalonde <olalonde@gmail.com>
2016-08-31 12:02:54 -0700

    index.js

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # eslint-config-blockai
+
+Base ESLint configuration for Blockai JavaScript projects.
+
+This is a slightly customised version of
+[eslint-config-airbnb-base](https://github.com/airbnb/javascript/tree/master/packages/eslint-config-airbnb-base).
---
+++ index.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: 'airbnb-base',
+  parser: 'babel-eslint',
+  rules: {
+    'consistent-return': 0,
+    'no-param-reassign': [2, { props: false }],
+    'semi': [2, 'never'],
+    'camelcase': 0,
+    'no-underscore-dangle': 0,
+  }
+}
